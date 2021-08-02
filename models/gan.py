283b2abff64220236f4064b83b7c22d95578aa1e
root <root@LAPTOP-A2EG90UI.localdomain>
2021-08-02 19:26:05 +0800

    update

--- models/gan.py
+++ models/gan.py
@@ -24,12 +24,27 @@ def Discriminator(z_size = 100, h1_size = 150, h2_size = 300, img_size = (28, 28
   h3 = tf.keras.layers.Dense(1, activation = tf.keras.activations.sigmoid, kernel_initializer = tf.keras.initializers.TruncatedNormal(stddev = 0.1))(h2); # h3.shape = (batch, 1)
   return tf.keras.Model(inputs = x, outputs = h3);
 
+def Trainer(z_size = 100, h1_size = 150, h2_size = 300, img_size = (28, 28), drop_rate = 0.2):
+
+  z_prior = tf.keras.Input((z_size,)); # z_prior.shape = (batch_z, 100)
+  x_generate = Generator(z_size = z_size, h1_size = h1_size, h2_size = h2_size, img_size = img_size)(z_prior);
+  x_nature = tf.keras.Input(img_size); # x_nature.shape = (batch_x, 28, 28)
+  x = tf.keras.layers.Concatenate(axis = 0)([x_generate, x_nature]);
+  pred = Discriminator(z_size = z_size, h1_size = h1_size, h2_size = h2_size, img_size = img_size, drop_rate = drop_rate)(x); # pred.shape = (batch_z + batch_x, 1)
+  # pred_generate.shape = (batch_z, 1), pred_nature.shape = (batch_x, 1)
+  pred_generate, pred_nature = tf.keras.layers.Lambda(lambda x: tf.split(x[0], [tf.shape(x[1])[0], tf.shape(x[2])[0]], axis = 0))([pred, z_prior, x_nature]);
+  return tf.keras.Model(inputs = (z_prior, x_nature), outputs = (pred_generate, pred_nature));
+
 if __name__ == "__main__":
 
-  inputs = np.random.normal(size = (4,100));
+  z_prior = np.random.normal(size = (4,100));
   generator = Generator();
   discriminator = Discriminator();
-  generated = generator(inputs);
+  generated = generator(z_prior);
   pred = discriminator(generated);
   generator.save('generator.h5');
   discriminator.save('discriminator.h5');
+  images = np.random.normal(size = (6, 28, 28));
+  trainer = Trainer();
+  pred_gen, pred_na = trainer([z_prior, images]);
+  print(pred_gen.shape, pred_na.shape);
