fdf1e0d3732df81645cdc16b82c5f7eda8a51aaa
root <root@LAPTOP-A2EG90UI.localdomain>
2021-08-02 19:57:48 +0800

    updaste

---
+++ create_dataset.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import tensorflow as tf;
+
+class MNIST(object):
+  def __init__(self,):
+    (self.train_x, _), (self.test_x, _) = tf.keras.datasets.mnist.load_data();
+  def sampler_generator(self, x):
+    def sampler():
+      for sample in x:
+        yield sample;
+      return;
+  def load_trainset(self,):
+    return tf.data.Dataset.from_generator(self.sampler_generator(self.train_x), (tf.uint8,), (tf.TensorShape([28, 28])));
+  def load_testset(self,):
+    return tf.data.Dataset.from_generator(self.sampler_generator(self.test_x), (tf.uint8,), (tf.TensorShape([28, 28])));
--- models/gan.py
+++ models/gan.py
@@ -33,7 +33,23 @@ def Trainer(z_size = 100, h1_size = 150, h2_size = 300, img_size = (28, 28), dro
   pred = Discriminator(z_size = z_size, h1_size = h1_size, h2_size = h2_size, img_size = img_size, drop_rate = drop_rate)(x); # pred.shape = (batch_z + batch_x, 1)
   # pred_generate.shape = (batch_z, 1), pred_nature.shape = (batch_x, 1)
   pred_generate, pred_nature = tf.keras.layers.Lambda(lambda x: tf.split(x[0], [tf.shape(x[1])[0], tf.shape(x[2])[0]], axis = 0))([pred, z_prior, x_nature]);
-  return tf.keras.Model(inputs = (z_prior, x_nature), outputs = (pred_generate, pred_nature));
+  d_loss = tf.keras.layers.Lambda(lambda x: - (tf.math.log(tf.math.reduce_mean(x[1], axis = 0)) + tf.math.log(1 - tf.math.reduce_mean(x[0], axis = 0))))([pred_generate, pred_nature]);
+  g_loss = tf.keras.layers.Lambda(lambda x: - tf.math.log(tf.math.reduce_mean(x, axis = 0)))(pred_generate);
+  return tf.keras.Model(inputs = (z_prior, x_nature), outputs = (d_loss, g_loss));
+
+def parse_function_generator(z_size = 100):
+  def parse_function(sample):
+    sample = tf.cast(sample, dtype = tf.float32);
+    sample = sample / 255. * 2 - 1; # sample range in [-1, 1]
+    # z_prior.shape = (100,), sample.shape = (28, 28)
+    return (tf.random.normal(shape = (z_size,)), sample), (0, 0);
+  return parse_function;
+
+def d_loss(_, d_loss):
+  return d_loss;
+
+def g_loss(_, g_loss):
+  return g_loss;
 
 if __name__ == "__main__":
 
@@ -46,5 +62,7 @@ if __name__ == "__main__":
   discriminator.save('discriminator.h5');
   images = np.random.normal(size = (6, 28, 28));
   trainer = Trainer();
-  pred_gen, pred_na = trainer([z_prior, images]);
-  print(pred_gen.shape, pred_na.shape);
+  d_loss, g_loss = trainer([z_prior, images]);
+  trainer.save('trainer.h5');
+  print(d_loss.shape, g_loss.shape);
+
