fd264a75b4bc8de7daa9877ffd7e99f1371e8f53
root <root@LAPTOP-A2EG90UI.localdomain>
2021-08-02 20:19:59 +0800

    update

---
+++ train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from os.path import join, exists;
+from absl import app, flags;
+import tensorflow as tf;
+from models import *;
+from create_dataset import *;
+
+FLAGS = flags.FLAGS;
+flags.DEFINE_enum('model', default = 'gan', enum_values = ['gan',], help = 'models to train');
+flags.DEFINE_integer('batch_size', default = 128, help = 'batch size');
+
+def main(unused_argv):
+
+  if exists(join('checkpoints', FLAGS.model)):
+    if FLAGS.model == 'gan':
+      custom_objects = {'tf': tf, 'd_loss': gan.d_loss, 'g_loss': gan.g_loss};
+    else:
+      raise Exception('unknown model!');
+    model = tf.keras.models.load_model(join('checkpoints', FLAGS.model), custom_objects = custom_objects, compile = True);
+    optimizer = model.optimizer;
+  else
+    if FLAGS.model == 'gan':
+      model = gan.Trainer();
+      dataset = MNIST();
+      trainset = dataset.load_trainset().map(gan.parse_function_generator()).batch(FLAGS.batch_size);
+      testset = dataset.load_testset().map(gan.parse_function_generator()).batch(FLAGS.batch_size);
+    else:
+      raise Exception('unknown model!');
+    optimizer = tf.keras.optimizers.Adam(tf.keras.optimizers.ExponentialDecay(1e-4, decay_steps = 20000, decay_rate = 0.97));
+    model.compile(optimizer = optimizer,
+                  loss = [gan.d_loss, gan.g_loss],
+                  metrics = [tf.keras.metrics.Mean(), tf.keras.metrics.Mean()]);
+    callbacks = [
+      tf.keras.callbacks.TensorBoard(log_dir = join('checkpoints', FLAGS.model)),
+      tf.keras.callbacks.ModelCheckpoint(filepath = join('checkpoints', FLAGS.model), save_freq = 1000)
+    ];
+    model.fit(trainset, epochs = 500, validation_data = testset, callbacks = callbacks);
+
+if __name__ == "__main__":
+
+  app.run(main);
