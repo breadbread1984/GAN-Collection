555ee47ce62d72b3d2751a62e17cf5da90f15f99
yi.xie <yi.xie@enflame-tech.com>
2021-10-12 16:40:18 +0800

    update

--- datasets.py
+++ datasets.py
@@ -10,6 +10,8 @@ import tensorflow as tf;
 class MNIST(object):
   def __init__(self,):
     (self.train_x, _), (self.test_x, _) = tf.keras.datasets.mnist.load_data();
+    self.train_x = tf.random.shuffle(self.train_x);
+    self.test_x = tf.random.shuffle(self.test_x);
   def load_trainset(self,):
     return tf.data.Dataset.from_tensor_slices(self.train_x);
   def load_testset(self,):
